d289ae3903a3c552f4a05e34955032f1cfbe1fca
hjfritz <fritzhj@gmail.com>
2022-09-11 21:27:38 -0700

    Primitive turning implemented

--- Hoverbike/Assets/Scripts/Jets.cs
+++ Hoverbike/Assets/Scripts/Jets.cs
@@ -8,25 +8,47 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Jets : MonoBehaviour
 {
 
-    [SerializeField] private int speed = 100;
+    [SerializeField] private float speed = .5f;
+    [SerializeField] private int turnSpeed = 3000;
     [SerializeField] private InputActionReference buttonPress;
-    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private InputActionReference leftTrigger;
+    [SerializeField] private InputActionReference rightTrigger;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private GameObject turnParent;
     
     private bool _forward = false;
+    private bool _left = false;
+    private bool _right = false;
     
     // Start is called before the first frame update
     void Start()
     {
         buttonPress.action.performed += Move;
         buttonPress.action.canceled += Move;
+        leftTrigger.action.performed += TurnLeft;
+        leftTrigger.action.canceled += TurnLeft;
+        rightTrigger.action.performed += TurnRight;
+        rightTrigger.action.canceled += TurnRight;
     }
-
+    
     // Update is called once per frame
     void FixedUpdate()
     {
         if (_forward)
         {
-            _rb.AddForceAtPosition(transform.TransformDirection(Vector3.forward) * speed, transform.localPosition);
+            rb.AddForceAtPosition(transform.TransformDirection(Vector3.forward) * speed, transform.localPosition);
+        }
+        
+        rb.AddTorque(rb.transform.TransformDirection((Vector3.up)) * -turnSpeed);
+        
+        if (_left)
+        {
+            turnParent.transform.Rotate(0,-1,0, Space.Self);
+        }
+
+        if (_right)
+        {
+            turnParent.transform.Rotate(0,1,0, Space.Self);
         }
     }
     
@@ -41,6 +63,34 @@ public class Jets : MonoBehaviour
         else
         {
             _forward = true;
+        }
+    }
+    
+    private void TurnLeft(InputAction.CallbackContext obj)
+    {
+        Debug.Log("pressedL");
+        
+        if (_left)
+        {
+            _left = false;
+        }
+        else
+        {
+            _left = true;
+        }
+    }
+    
+    private void TurnRight(InputAction.CallbackContext obj)
+    {
+        Debug.Log("pressedR");
+        
+        if (_right)
+        {
+            _right = false;
+        }
+        else
+        {
+            _right = true;
         }
     }
 }
